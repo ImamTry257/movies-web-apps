9a33d7e418ce3986bc9f520006ea16cdc75721f1
imamtry <imamtrywibowo@gmail.com>
2024-02-03 22:28:15 +0700

    header is ok

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -19,10 +19,6 @@ class Header extends React.Component {
             console.log('test disini', navLinks)
             navLinks.classList.toggle("show1");
         }
-
-        // $('#parent-menu').hover(() => {
-        //     console.log(htmlcssArrow)
-        // })
     }
 
     render() {
@@ -30,30 +26,30 @@ class Header extends React.Component {
             <nav style={{ backgroundColor: '#fff' }}>
                 <div className="navbar">
                     <i className='bx bx-menu'></i>
-                    <div className="logo"><a href="#">MOVIES WEB APP</a></div>
+                    <div className="logo"><a href="#">Nonton Yuk</a></div>
                     <div className="nav-links">
                         <div className="sidebar-logo">
                             <span className="logo-name">Logo</span>
                             <i className='bx bx-x' ></i>
                         </div>
                         <ul className="links">
-                            <li><a href="">HOME</a></li>
+                            <li><a href="">Home</a></li>
                             <li id="parent-menu">
-                                <a href="#">MENU</a>
+                                <a href="#">Genre</a>
                                 <box-icon type="regular" name="chevron-down" className="boxiconcus" id="htmlcss-arrow"></box-icon>
 
                                 {/* <i className='bx bxs-chevron-down htmlcss-arrow arrow'></i> */}
                                 <ul className="htmlCss-sub-menu sub-menu">
-                                    <li><a href="">CP, TP & ATP</a></li>
-                                    <li><a href="">Topik/Materi</a></li>
-                                    <li><a href="">Potensi Lokal Gudeg</a></li>
-                                    <li><a href="">Refleksi</a></li>
+                                    <li><a href="">Action</a></li>
+                                    <li><a href="">Advanture</a></li>
+                                    <li><a href="">Animasi</a></li>
+                                    <li><a href="">Crime</a></li>
                                 </ul>
                             </li>
-                            <li><a href="">KONTAK</a></li>
-                            <li><a href="">PROFILE</a></li>
-                            <li><a className="btn btn-dark-register text-white d-flex align-items-center h-50 d-flex align-items-center h-50" href="">DAFTAR</a></li>
-                            <li><a className="btn btn-outline-secondary d-flex align-items-center h-50" href="">LOGIN</a></li>
+
+
+                            <li><a href="">Populer</a></li>
+                            <li><a href="">Negara</a></li>
 
                         </ul>
                     </div>
